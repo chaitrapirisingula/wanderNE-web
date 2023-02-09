34eea0cc67eb3ab7590da892c207d3ab29cbb04a
chaitrapirisingula <89547692+chaitrapirisingula@users.noreply.github.com>
2023-02-09 17:47:32 -0600

    layout

--- src/Card.js
+++ src/Card.js
@@ -8,12 +8,12 @@ function Card( { props } ) {
 
   return (
     <div className="card"> 
-        <div className="card_body">
-            <img className="card_image" src={props.image} alt=""></img>
-            <h3 className="card_title">{props.name}</h3>
-            <p className="card_description">{props.address.city}, {props.address.state}</p>
-        </div>
+      <div className="card_body">
+        <img className="card_image" src={props.image} alt=""></img>
+        <h3 className="card_title">{props.name}</h3>
+        <p className="card_description">{props.address.city}, {props.address.state}</p>
         <button className="card_button" onClick={() => {navigate("/sites/" + props.name, { state: props });}}>View More</button>
+      </div>
     </div>
   );
 }
