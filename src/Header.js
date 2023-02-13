29fc44b3716e5780bcf38318c2d339983a866ef1
chaitrapirisingula <89547692+chaitrapirisingula@users.noreply.github.com>
2023-02-12 19:38:18 -0600

    replace sidebar with header

--- src/Header.js
+++ src/Header.js
@@ -1,58 +1,27 @@
-import React, { useState } from "react";
-import { ReactComponent as BigLogo } from './biglogo.svg';
-import { ReactComponent as Logo } from './logo.svg';
-import { ProSidebar, Menu, MenuItem, SidebarHeader, SidebarFooter, SidebarContent } from "react-pro-sidebar";
+import {  Menu, MenuItem } from "react-pro-sidebar";
 import { FaList, FaMap } from "react-icons/fa";
-import { FiHome, FiArrowLeftCircle, FiArrowRightCircle } from "react-icons/fi";
+import { FiHome } from "react-icons/fi";
 import { useNavigate } from "react-router-dom";
-import "react-pro-sidebar/dist/css/styles.css";
+import WanderLogo from "./WanderNebraskaLogo.png"
 import "./Header.css";
 
 
 const Header = () => {
 
     let navigate = useNavigate();
-  
-    const [menuCollapse, setMenuCollapse] = useState(false)
-
-    const menuIconClick = () => {
-        menuCollapse ? setMenuCollapse(false) : setMenuCollapse(true);
-    };
 
     return (
-        <>
-        <div id="header">
-            <ProSidebar collapsed={menuCollapse}>
-            <SidebarHeader>
-                <div className="logotext">
-                <p>{menuCollapse ? <Logo /> : <BigLogo />}</p>
-                </div>
-                <div className="closemenu" onClick={menuIconClick}>
-                {menuCollapse ? (
-                    <FiArrowRightCircle/>
-                ) : (
-                    <FiArrowLeftCircle/>
-                )}
-                </div>
-            </SidebarHeader>
-            <SidebarContent>
-                <Menu iconShape="square">
+        <header className="main_header">
+            <img src={WanderLogo} className="logo"/>
+            <Menu iconShape="square">
                 <MenuItem icon={<FiHome />} onClick={() => {navigate("/");}}>Home</MenuItem>
                 <MenuItem icon={<FaList />} onClick={() => {navigate("/sites");}}>Sites</MenuItem>
                 <MenuItem icon={<FaMap />} onClick={() => {navigate("/map");}}>Map</MenuItem>
-                </Menu>
-            </SidebarContent>
-            <SidebarFooter>
-                <Menu iconShape="square">
-                <div className="logotext">
-                    <p style={{color: "#000080"}}>
-                    {menuCollapse ? "nshsf": "Nebraska State Historical Society Foundation"}</p>
-                </div>
-                </Menu>
-            </SidebarFooter>
-            </ProSidebar>
-        </div>
-        </>
+            </Menu>
+            <a href={"https://nshsf.org"}><img className="nshsf_logo" src=
+            "https://www.nshsf.org/wp-content/uploads/2021/02/NSHSF_Logo-e1552276800426.gif" 
+            alt="Nebraska State Historical Society Foundation"></img></a>
+        </header>
     );
 };
 
--- src/Site.js
+++ src/Site.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import { Helmet } from "react-helmet";
 import "./App.css";
 
 function Site() {
 
   let navigate = useNavigate();
+
   const location = useLocation();
   
   return (
@@ -14,20 +15,27 @@ function Site() {
           <meta charSet="utf-8" />
           <title>{location.state.name}</title>
       </Helmet>
-      <h1>{location.state.name}</h1>
-      <button className="back_button" onClick={() => {navigate("/sites");}}>Back to Sites</button>
-      <p>{location.state.hours}</p>
-      <div className="site_info">
-        <p>{location.state.phone}</p>
-        <p>{location.state.link}</p>
-        <p>{location.state.address.streetAddress}, {location.state.address.city}, {location.state.address.state}, {location.state.address.postalCode}</p>
+      <div className = "head_text">
+        <div className = "head_image">
+          <img className="site_image" src={location.state.image} alt=""></img>
+        </div>
+        <div class='text_on_image'>
+          <h1>{location.state.name}</h1>
+        </div>
       </div>
-      <div className="site_description">
-        <img className="site_image" src={location.state.image} alt=""></img>
-        <p>{location.state.description}</p>
-        <iframe title="map" className="site_map" src={`https://maps.google.com/maps?q=${location.state.name}
-        &t=&z=13&ie=UTF8&iwloc=&output=embed`}></iframe>
-      </div>
+      <section className="site_body">
+        <p>{location.state.hours}</p>
+        <div className="site_info">
+          <p>{location.state.phone}</p>
+          <a href={"https://" + location.state.link}>{location.state.link}</a>
+          <p>{location.state.address.streetAddress}, {location.state.address.city}, {location.state.address.state}, {location.state.address.postalCode}</p>
+        </div>
+        <div className="site_description">
+          <p>{location.state.description}</p>
+          <iframe title="map" className="site_map" src={`https://maps.google.com/maps?q=${location.state.name}
+          &t=&z=13&ie=UTF8&iwloc=&output=embed`}></iframe>
+        </div>
+      </section>
     </div>
   );
 }
