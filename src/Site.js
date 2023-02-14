502d362a677688d6a127675ee53a05d481a1e583
chaitrapirisingula <89547692+chaitrapirisingula@users.noreply.github.com>
2023-02-13 22:34:10 -0600

    update styling

--- src/Site.js
+++ src/Site.js
@@ -15,27 +15,23 @@ function Site() {
           <meta charSet="utf-8" />
           <title>{location.state.name}</title>
       </Helmet>
-      <div className = "head_text">
-        <div className = "head_image">
-          <img className="site_image" src={location.state.image} alt=""></img>
-        </div>
-        <div class='text_on_image'>
-          <h1>{location.state.name}</h1>
-        </div>
-      </div>
+      <img className="site_image" src={location.state.image} alt=""></img>
       <section className="site_body">
-        <button className="card_button" onClick={() => {navigate("/sites");}}>Back</button>
-        <p>{location.state.hours}</p>
+        <div>
+          <h1>{location.state.name}</h1>
+          <button className="back_button" onClick={() => {navigate("/sites");}}>Back</button>
+          <p>{location.state.hours}</p>
+        </div>
         <div className="site_info">
           <p>{location.state.phone}</p>
           <a href={"https://" + location.state.link}>{location.state.link}</a>
           <p>{location.state.address.streetAddress}, {location.state.address.city}, {location.state.address.state}, {location.state.address.postalCode}</p>
         </div>
-        <div className="site_description">
+        {/* <div className="site_description"> */}
           <p>{location.state.description}</p>
           <iframe title="map" className="site_map" src={`https://maps.google.com/maps?q=${location.state.name}
           &t=&z=13&ie=UTF8&iwloc=&output=embed`}></iframe>
-        </div>
+        {/* </div> */}
       </section>
     </div>
   );
